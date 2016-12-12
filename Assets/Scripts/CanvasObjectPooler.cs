13d3031659e837b7440becef0cb50d8cf67fbcc9
Braidfifteen <raatkin15@gmail.com>
2016-12-12 01:02:58 -0500

    See previous commit

---
+++ Assets/Scripts/CanvasObjectPooler.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class CanvasObjectPoolItem
+{
+    public int amountToPool;
+    public GameObject objectToPool;
+    public bool shouldExpand;
+}
+
+public class CanvasObjectPooler : MonoBehaviour
+{ 
+    public static CanvasObjectPooler SharedInstance;
+    public List<CanvasObjectPoolItem> itemsToPool;
+    public List<GameObject> pooledObjects;
+    
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        foreach (CanvasObjectPoolItem item in itemsToPool)
+        {
+            for (int i = 0; i < item.amountToPool; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
+        }
+    }
+
+    public GameObject GetPooledObject(string tag)
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy && pooledObjects[i].tag == tag)
+                return pooledObjects[i];
+        }
+        foreach (CanvasObjectPoolItem item in itemsToPool)
+        {
+            if (item.objectToPool.tag == tag)
+            {
+                if (item.shouldExpand)
+                {
+                    GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                    obj.SetActive(false);
+                    pooledObjects.Add(obj);
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/TESTDISABLEANIMATION.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TESTDISABLEANIMATION : MonoBehaviour
+{
+    public GameObject parent;
+    public void Disable()
+    {
+        parent.SetActive(false);
+    }
+
+    //public void parentDisable()
+    //{
+
+    //}
+}
